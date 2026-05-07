bf3ab8c72afe9979e8c29224dc4cc2ee3d142c24
MathVic10r <matheus.victortecn@gmail.com>
2026-05-07 20:49:53 -0300

    Adição de projeto

---
+++ Edu/matheus/firstweek/classe.java
@@ -0,0 +1,36 @@
+public class boletimEstudante{
+
+public static void main (String[]args){
+
+    int mediaFinal = 5;
+    if (mediaFinal < 6)
+        System.out.println("Reprovado");
+
+   else if (mediaFinal == 6)
+        System.out.println("Recuperacao");
+
+    else
+      
+        System.out.println("Aprovado");
+    }
+}
+//Int, Str, bool, float
+//Declaração de variaveis não pode ter caracteres, numeros e simbolos//
+//Deve ter letras e underline _, $ ou 
+//Deve iniciar sempre por Letra minusculas, jamais numeros
+//Não popde haver espaços
+//Não pode haver palavras reservadas
+
+//Declaração de variável
+//tipo NomeBemDefinido = ()
+//int old = 23;
+//double altura = 1.62;
+//string NomeDog = "Stuart";
+//int old = 11; 
+//boolean YesOrNo = true;
+//anoFabricacao = 2016;
+//Identação basicamente é um termo utilizado para escrever o código do programa de forma hierarquica e organizada afim de facilitar a leitura do código;
+//Variável deve sempre estar no singular
+//Em lingua unica - PT, EN, ES...
+//Não recomendado abreviação
+//Métodos devem ser nomeados como verbos, atraves da mistura de maiusculas e minusculas. Ex: somar, abrirCaixa, concluirProcesso...
